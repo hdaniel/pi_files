0e155de4ad6b601e30de8a5d58ddaace40fbe67f
hdaniel <harold.m.daniel@gmail.com>
2018-08-24 15:19:01 -0500

    Remove file_list.txt if no changes.

--- clean_files.sh
+++ clean_files.sh
@@ -71,6 +71,9 @@ do
         mv file_list.txt file_list_processed.txt
         chown ftpuser:ftpgroup file_list_processed.txt
       fi
+      if [ ! -f "file_list.txt" ]; then
+        rm file_list.txt
+      fi
       cd ..
     done
 
